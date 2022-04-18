101ac78f5b69b77e3ed8983e73e95ceded48b302
Ashwin <ashwinjoseph95@gmail.com>
2022-04-18 18:40:21 +0530

    Catkin workspace Repo first push

---
+++ build/ros_essentials_cpp/CMakeFiles/talker_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/ashwin/catkin_ws/devel/lib/ros_essentials_cpp/talker_node"
+  "/home/ashwin/catkin_ws/devel/lib/ros_essentials_cpp/talker_node.pdb"
+  "CMakeFiles/talker_node.dir/src/topic01_basics/talker_listener/talker.cpp.o"
+  "CMakeFiles/talker_node.dir/src/topic01_basics/talker_listener/talker.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ devel/share/gennodejs/ros/ros_essentials_cpp/msg/_index.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+let IoTSensor = require('./IoTSensor.js');
+let FibonacciActionGoal = require('./FibonacciActionGoal.js');
+let FibonacciResult = require('./FibonacciResult.js');
+let FibonacciActionResult = require('./FibonacciActionResult.js');
+let FibonacciAction = require('./FibonacciAction.js');
+let FibonacciActionFeedback = require('./FibonacciActionFeedback.js');
+let FibonacciFeedback = require('./FibonacciFeedback.js');
+let FibonacciGoal = require('./FibonacciGoal.js');
+
+module.exports = {
+  IoTSensor: IoTSensor,
+  FibonacciActionGoal: FibonacciActionGoal,
+  FibonacciResult: FibonacciResult,
+  FibonacciActionResult: FibonacciActionResult,
+  FibonacciAction: FibonacciAction,
+  FibonacciActionFeedback: FibonacciActionFeedback,
+  FibonacciFeedback: FibonacciFeedback,
+  FibonacciGoal: FibonacciGoal,
+};
