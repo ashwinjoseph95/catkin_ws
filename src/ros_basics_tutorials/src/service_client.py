6bb8b34172a53daedd2731bba4bae4eafc075a62
Ashwin <ashwinjoseph95@gmail.com>
2022-04-30 13:20:28 +0530

    Updating with working and verified service request response scripts

--- src/ros_basics_tutorials/src/service_client.py
+++ src/ros_basics_tutorials/src/service_client.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python
+
 import sys
 import rospy
-#from std_msgs.msg import String
+from ros_essentials_cpp.srv import AddTwoInts
+from ros_essentials_cpp.srv import AddTwoIntsRequest
+from ros_essentials_cpp.srv import AddTwoIntsResponse
 
-from ros_basics_tutorials.srv import AddTwoInts
-from ros_basics_tutorials.srv import AddTwoIntsRequest
-from ros_basics_tutorials.srv import AddTwoIntsResponse
-
-def add_two_ints_client(x,y):
+def add_two_ints_client(x, y):
     rospy.wait_for_service('add_two_ints')
     try:
-        add_two_ints = rospy.ServiceProxy('add_two_ints',AddTwoInts)
-        resp1=add_two_ints(x,y)
+        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
+        resp1 = add_two_ints(x, y)
         return resp1.sum
-    except rospy.ServiceException(e):
-        print("Service Call Failed: {}".format(e)
+    except rospy.ServiceException as e:
+        print ("Service call failed: {}".format(e))
 
+def usage():
+    return "%s [x y]"%sys.argv[0]
 
-
-if __name__=="__main__":
-    rospy.init_node("add_two_ints_entcli")
-    s=rospy.Service("add_two_ints", AddTwoInts, handle_add_two_ints)
-    print("Ready to add two ints")
-    rospy.spin()
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+    else:
+        print (usage())
+        sys.exit(1)
+    print ("Requesting %s+%s"%(x, y))
+    s = add_two_ints_client(x, y)
+    print ("%s + %s = %s"%(x, y, s))
