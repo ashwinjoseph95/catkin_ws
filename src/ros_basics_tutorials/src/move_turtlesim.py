58944d77f30f4141ef79b95aa73c628fecb93043
Ashwin <ashwinjoseph95@gmail.com>
2022-05-07 00:10:59 +0530

    Updating rotate function with python

--- src/ros_basics_tutorials/src/move_turtlesim.py
+++ src/ros_basics_tutorials/src/move_turtlesim.py
@@ -60,7 +60,50 @@ def move(speed, distance, is_forward):
         velocity_message.linear.x =0
         velocity_publisher.publish(velocity_message)
     
+def rotate (angular_speed_degree, relative_angle_degree, clockwise):
+    
+    global yaw
+    velocity_message = Twist()
+    velocity_message.linear.x=0
+    velocity_message.linear.y=0
+    velocity_message.linear.z=0
+    velocity_message.angular.x=0
+    velocity_message.angular.y=0
+    velocity_message.angular.z=0
 
+    #get current location 
+    theta0=yaw
+    angular_speed=math.radians(abs(angular_speed_degree))
+
+    if (clockwise):
+        velocity_message.angular.z =-abs(angular_speed)
+    else:
+        velocity_message.angular.z =abs(angular_speed)
+
+    angle_moved = 0.0
+    loop_rate = rospy.Rate(10) # we publish the velocity at 10 Hz (10 times a second)    
+    cmd_vel_topic='/turtle1/cmd_vel'
+    velocity_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
+
+    t0 = rospy.Time.now().to_sec()
+
+    while True :
+        rospy.loginfo("Turtlesim rotates")
+        velocity_publisher.publish(velocity_message)
+
+        t1 = rospy.Time.now().to_sec()
+        current_angle_degree = (t1-t0)*angular_speed_degree
+        loop_rate.sleep()
+
+
+                       
+        if  (current_angle_degree>relative_angle_degree):
+            rospy.loginfo("reached")
+            break
+
+    #finally, stop the robot when the distance is moved
+    velocity_message.angular.z =0
+    velocity_publisher.publish(velocity_message)
 
 
 if __name__ == '__main__':
@@ -76,7 +119,8 @@ if __name__ == '__main__':
         pose_subscriber = rospy.Subscriber(position_topic, Pose, poseCallback) 
         time.sleep(2)
 
-        move(1.0, 2.0, False)
+        #move(1.0, 2.0, False)
+        rotate(30, 90, True)
        
     except rospy.ROSInterruptException:
         rospy.loginfo("node terminated.")
